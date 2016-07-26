ef828597f52f43ec7ee1ca23a50249d1121236b8
yuan <1406046087@qq.com>
2016-07-26 23:38:40 +0800

    【EDIT】添加ＡBaseActivity抽象类（暂时不用）

--- demo/src/main/java/yuan/demo/UserIfoActivity.java
+++ demo/src/main/java/yuan/demo/UserIfoActivity.java
@@ -1,17 +1,26 @@
 package yuan.demo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import yuan.demo.databinding.ActivityUserIfoBinding;
+
 public class UserIfoActivity extends AppCompatActivity {
+
+    ActivityUserIfoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_ifo);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_user_ifo);
+        initView();
+    }
 
-
+    private void initView() {
 
 
     }
+
+
 }
