c37aa563e121314917efc49c788380e9adf384d1
yuan <1406046087@qq.com>
2016-07-26 23:55:58 +0800

    【EDIT】实现到用户信息的跳转

--- demo/src/main/java/yuan/demo/SettingActivity.java
+++ demo/src/main/java/yuan/demo/SettingActivity.java
@@ -20,16 +20,36 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding= DataBindingUtil.setContentView(this,R.layout.activity_setting);
 
+        initView();
+
+    }
+
+    private void initView() {
+
+
+        //二维码
         binding.ivUserRqcode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //TODO 添加 ID值　参数
                 Intent intent=new Intent(SettingActivity.this,RqcodeActivity.class);
                 startActivity(intent);
             }
         });
+
+        //用户信息
+        binding.settingUserIfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO 添加用户对象　参数
+                Intent intent=new Intent(SettingActivity.this,UserIfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 
-    private void onClick(){
-
-    }
 }
