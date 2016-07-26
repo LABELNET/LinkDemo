53da8f94ddf61c5e3d5559f02f8cba66c756f52d
yuan <1406046087@qq.com>
2016-07-26 17:59:57 +0800

    【EDIT】添加二维码生成页面

--- demo/src/main/java/yuan/demo/RqcodeActivity.java
+++ demo/src/main/java/yuan/demo/RqcodeActivity.java
@@ -3,11 +3,14 @@ package yuan.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * 二维码生成页面
+ */
+public class RqcodeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_rqcode);
     }
 }
--- demo/src/main/java/yuan/demo/SettingActivity.java
+++ demo/src/main/java/yuan/demo/SettingActivity.java
@@ -3,6 +3,9 @@ package yuan.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 个人设置页面
+ */
 public class SettingActivity extends AppCompatActivity {
 
     @Override
