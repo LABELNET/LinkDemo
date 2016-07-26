dfa0a257bce83df5ef4caeded868e71aecbe9bd0
yuan <1406046087@qq.com>
2016-07-27 00:47:34 +0800

    【EDIT】修改页面添加菜单

--- demo/src/main/java/yuan/demo/UpdateIfoActivity.java
+++ demo/src/main/java/yuan/demo/UpdateIfoActivity.java
@@ -3,7 +3,12 @@ package yuan.demo;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import yuan.demo.databinding.ActivityUpdateIfoBinding;
 
@@ -16,24 +21,52 @@ public class UpdateIfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding= DataBindingUtil.setContentView(this,R.layout.activity_update_ifo);
         initView();
-
-
     }
 
     private void initView() {
-      //焦点监听
-      binding.etUpdateIfo.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-          @Override
-          public void onFocusChange(View view, boolean b) {
-              if(b){
-                  setClearIconIsShow(getEditText().isEmpty());
-              }else{
-                  setClearIconIsShow(false);
+
+          binding.etUpdateIfo.addTextChangedListener(new TextWatcher() {
+              @Override
+              public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+              @Override
+              public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                  setClearIconIsShow(charSequence.length()>0);
               }
+              @Override
+              public void afterTextChanged(Editable editable) {
+                  //TODO 可以进行判断输入的文字长度
+                   setClearIconIsShow(editable.toString().length()>0);
+              }
+          });
+
+        //焦点监听
+        binding.etUpdateIfo.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(b){
+                    setClearIconIsShow(getEditText().length()>0);
+                }else{
+                    setClearIconIsShow(false);
+                }
+            }
+        });
+
+      binding.ivUpdateIfoClear.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              clearEditText();
           }
       });
+
     }
 
+
+    /**
+     * 清除输入框信息
+     */
+    private void clearEditText() {
+        binding.etUpdateIfo.setText("");
+    }
 
     /**
      *　获取输入框信息
@@ -54,5 +87,28 @@ public class UpdateIfoActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_save,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_save:
+                saveData();
+                break;
+        }
+        return true;
+    }
+
+    /**
+     * 保存数据
+     */
+    private void saveData() {
+        Toast.makeText(this,"保存",Toast.LENGTH_LONG).show();
+    }
 
 }
