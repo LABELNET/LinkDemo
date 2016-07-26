2f68b970e8f55d6f279c99972418a55fb1dd8e5e
yuan <1406046087@qq.com>
2016-07-26 20:35:31 +0800

    【EDIT】二维码界面基本实现

--- demo/src/main/java/yuan/demo/SettingActivity.java
+++ demo/src/main/java/yuan/demo/SettingActivity.java
@@ -1,16 +1,35 @@
 package yuan.demo;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import yuan.demo.databinding.ActivitySettingBinding;
 
 /**
  * 个人设置页面
  */
 public class SettingActivity extends AppCompatActivity {
 
+    ActivitySettingBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_setting);
+
+        binding.ivUserRqcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(SettingActivity.this,RqcodeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void onClick(){
+
     }
 }
