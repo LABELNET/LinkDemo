9de8fb0e4743b6400cd0b5b2b45451b031c345d0
yuan <1406046087@qq.com>
2016-07-27 00:13:26 +0800

    【EDIT】优化二维码界面实现

--- demo/src/main/java/yuan/demo/RqcodeActivity.java
+++ demo/src/main/java/yuan/demo/RqcodeActivity.java
@@ -1,19 +1,36 @@
 package yuan.demo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import yuan.demo.databinding.ActivityRqcodeBinding;
 
 /**
  * 二维码生成页面
  */
 public class RqcodeActivity extends AppCompatActivity {
 
+    ActivityRqcodeBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rqcode);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_rqcode);
+        initView();
+
     }
 
+    private void initView() {
+
+        binding.ivRqcodeClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
 
     @Override
     public void onBackPressed() {
