96e37239e6f1d18bf975c0e4b2a1ce7cc74b332a
yuan <1406046087@qq.com>
2016-07-28 00:35:43 +0800

    【EDIT】添加支付设置页面

---
+++ demo/src/main/java/yuan/demo/SafetyPaySettingActivity.java
@@ -0,0 +1,16 @@
+package yuan.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 支付设置页面
+ */
+public class SafetyPaySettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safety_pay_setting);
+    }
+}
