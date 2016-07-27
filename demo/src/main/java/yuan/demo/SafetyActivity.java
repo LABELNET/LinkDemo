26b4aef541578917677aaef7947b45573be1a809
yuan <1406046087@qq.com>
2016-07-27 10:21:24 +0800

    【EDIT】账户与安全界面框架搭建

--- demo/src/main/java/yuan/demo/SafetyActivity.java
+++ demo/src/main/java/yuan/demo/SafetyActivity.java
@@ -3,13 +3,14 @@ package yuan.demo;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import yuan.demo.databinding.ActivitySafetyBinding;
 
 /**
  * 账户与安全界面
  */
-public class SafetyActivity extends AppCompatActivity {
+public class SafetyActivity extends AppCompatActivity implements View.OnClickListener{
 
     ActivitySafetyBinding binding;
 
@@ -18,12 +19,39 @@ public class SafetyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding= DataBindingUtil.setContentView(this,R.layout.activity_safety);
         initData();
+        initView();
+    }
+
+    private void initView() {
+
     }
 
 
     private void initData() {
         //电话号码与锁屏手势状态
     }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.safety_phone:
+                //修改手机号
+                break;
+            case R.id.safety_login_password:
+                break;
+            case R.id.safety_pay_setting:
+                break;
+            case R.id.safety_lock_gesture:
+                break;
+            case R.id.safety_secret_backup:
+                break;
+        }
+    }
+
+
+
+
 
 
 }
