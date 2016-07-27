88da23dbae03f62987b0935948dbb156cb8dceb4
yuan <1406046087@qq.com>
2016-07-27 14:46:26 +0800

    【EDIT】修改密码界面实现

---
+++ demo/src/main/java/yuan/demo/SafetyPasswordActivity.java
@@ -0,0 +1,19 @@
+package yuan.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 修改登陆密码界面
+ *
+ */
+public class SafetyPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safety_password);
+    }
+
+
+}
