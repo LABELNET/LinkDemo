57e49e3c60d6cd614ca2d6bf135052cd4af9089d
yuan <1406046087@qq.com>
2016-07-28 23:58:58 +0800

    【EDIT】添加私钥备份导出

---
+++ demo/src/main/java/yuan/demo/SafetyPrivateKeyBackup.java
@@ -0,0 +1,16 @@
+package yuan.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 私钥备份导出
+ */
+public class SafetyPrivateKeyBackup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safety_private_key_backup);
+    }
+}
