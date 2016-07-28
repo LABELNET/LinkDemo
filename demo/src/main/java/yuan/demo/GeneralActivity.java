d4f084d09ddb845db0be1f48e52e5fd28db4bcdf
yuan <1406046087@qq.com>
2016-07-29 01:18:50 +0800

    【EDIT】通用界面添加与初始化

---
+++ demo/src/main/java/yuan/demo/GeneralActivity.java
@@ -0,0 +1,32 @@
+package yuan.demo;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import yuan.demo.databinding.ActivityGeneralBinding;
+
+public class GeneralActivity extends AppCompatActivity {
+
+    ActivityGeneralBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_general);
+
+        initView();
+    }
+
+    private void initView() {
+
+        binding.generalMsgNotify.setState(true);
+        binding.generalMoreLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO 多语言跳转
+            }
+        });
+
+    }
+}
