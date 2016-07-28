180f81d4123c0b73014563acb00796f3ad9ec62e
yuan <1406046087@qq.com>
2016-07-28 22:18:03 +0800

    【EDIT】添加手势绘制页面

---
+++ demo/src/main/java/yuan/demo/SafeGestureDrawActivity.java
@@ -0,0 +1,13 @@
+package yuan.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SafeGestureDrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safe_gesture_draw);
+    }
+}
