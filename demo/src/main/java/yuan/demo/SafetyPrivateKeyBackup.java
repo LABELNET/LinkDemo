fdede77a49e0fddf3648e5732de93eef00c4ee14
yuan <1406046087@qq.com>
2016-07-29 00:06:20 +0800

    【EDIT】私钥导出备份界面初始化

--- demo/src/main/java/yuan/demo/SafetyPrivateKeyBackup.java
+++ demo/src/main/java/yuan/demo/SafetyPrivateKeyBackup.java
@@ -2,6 +2,9 @@ package yuan.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * 私钥备份导出
@@ -12,5 +15,28 @@ public class SafetyPrivateKeyBackup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safety_private_key_backup);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_private_key,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_save_mob:
+                show("保存到手机相册");
+                break;
+            case R.id.menu_send_mail:
+                show("发送到电子邮箱");
+                break;
+        }
+        return true;
+    }
+
+    private void show(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
     }
 }
