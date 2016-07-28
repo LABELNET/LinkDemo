5486d332a163279439852ba5fb28faf6399967a2
yuan <1406046087@qq.com>
2016-07-28 10:45:07 +0800

    【EDIT】添加手势密码Activity

---
+++ demo/src/main/java/yuan/demo/SafetyGesturePasswordActivity.java
@@ -0,0 +1,16 @@
+package yuan.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 手势密码
+ */
+public class SafetyGesturePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safety_gesture_password);
+    }
+}
