7cc2fb92752a08cdee7b7b3501ec892ce76fd8a8
yuan <1406046087@qq.com>
2016-07-29 01:03:18 +0800

    【EDIT】隐私页面实现并进行监听

--- demo/src/main/java/yuan/demo/PrivacyActivity.java
+++ demo/src/main/java/yuan/demo/PrivacyActivity.java
@@ -1,13 +1,36 @@
 package yuan.demo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PrivacyActivity extends AppCompatActivity {
+import yuan.demo.databinding.ActivityPrivacyBinding;
+import yuan.demo.weiget.SwitchView;
+
+public class PrivacyActivity extends AppCompatActivity{
+
+    ActivityPrivacyBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_privacy);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_privacy);
+        initView();
+    }
+
+    private void initView() {
+        binding.privacyAddSwitch.setState(true);
+        binding.privacySearchSwitch.setState(true);
+        binding.privacyAutoSwitch.setOnStateChangedListener(new SwitchView.OnStateChangedListener() {
+            @Override
+            public void toggleToOn() {
+
+            }
+
+            @Override
+            public void toggleToOff() {
+
+            }
+        });
     }
 }
