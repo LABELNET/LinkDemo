79e0e0fbdca0d1cd5d0ac66bea382e770805edc5
yuan <1406046087@qq.com>
2016-07-28 13:12:29 +0800

    【EDIT】添加手势密码业务

--- demo/src/main/java/yuan/demo/SafetyGesturePasswordActivity.java
+++ demo/src/main/java/yuan/demo/SafetyGesturePasswordActivity.java
@@ -3,8 +3,11 @@ package yuan.demo;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import yuan.demo.databinding.ActivitySafetyGesturePasswordBinding;
+import yuan.demo.weiget.SwitchView;
 
 /**
  * 手势密码
@@ -22,5 +25,26 @@ public class SafetyGesturePasswordActivity extends AppCompatActivity {
 
     private void initView() {
         binding.gestureOpen.setState(true);
+        binding.gestureOpen.setOnStateChangedListener(new SwitchView.OnStateChangedListener() {
+            @Override
+            public void toggleToOn() {
+                //TODO 有手势密码-参数key　打开锁屏绘制
+                binding.gestureOpen.setState(true);
+            }
+
+            @Override
+            public void toggleToOff() {
+                binding.gestureOpen.setState(false);
+            }
+        });
+
+        //TODO 没有手势密码　隐藏修改手势密码
+        binding.gestureUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO 修改手势密码-参数key 验证手势密码
+
+            }
+        });
     }
 }
